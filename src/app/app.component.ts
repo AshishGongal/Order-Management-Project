23da1233ab85ca3dd220b6258aa48a2725cedb9c
Ashish Gongal <113004449+AshishGongal@users.noreply.github.com>
2023-01-25 21:50:39 +0530

    Sprint2 Project Frontend
    OrderManagemnetSystem
    Frontend -> [Angular, Bootstrap]

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ServicedemoService } from './servicedemo.service';
+import { Student } from './student';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'spring_ang';
+  
+ 
+
+ 
+}
